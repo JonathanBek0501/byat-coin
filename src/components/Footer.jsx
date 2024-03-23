fa208ddd4612fb9d01d99d3ef707aeb8f95ab4b3
Shokhjakhon <shoxjahonsoliyev@gmail.com>
2024-03-23 16:28:46 +0500

    fix: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer className="bg-primary-lighter text-center px-8 sm:px-12 py-6">
-            <span className="text-2xl font-medium text-white">
+        <footer className="bg-primary-lighter border-t-4 border-primary text-center px-8 sm:px-12 py-6">
+            <span className="text-2xl font-semibold text-primary">
                 © 2024 FRONK
             </span>
         </footer>
